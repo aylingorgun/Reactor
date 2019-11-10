66053902b0e221e93ec52589ad99d6c8a87f2a68
Aylin Görgün <aylingrgn2@gmail.com>
2019-11-10 22:54:31 +0300

    Reactor initial commit.

---
+++ Reactor/Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Rigidbody2D bullet;
+    public Transform FirePoint;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        FirePoint = FirePoint.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire(){
+        Instantiate(bullet, FirePoint.position, Quaternion.identity);
+    }
+}
---
+++ Reactor/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Text text;
+    int lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = 1;
+        text.text = lives.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = lives.ToString();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("dfs");
+            //Destroy(collision.gameObject);
+            lives--;
+        }
+    }
+}
